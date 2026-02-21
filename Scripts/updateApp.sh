af81bf90e8f4e6f727f5b4222218f92a50b413a5
Ubuntu <ubuntu@mydashboard-micro>
2026-02-21 22:35:56 +0000

    adding update script

---
+++ Scripts/updateApp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Move to your app directory
+cd /home/ubuntu/MyGarminDash
+
+# Check for updates from GitHub
+git fetch
+
+# Compare local main branch with remote main branch
+LOCAL=$(git rev-parse HEAD)
+REMOTE=$(git rev-parse @{u})
+
+if [ $LOCAL != $REMOTE ]; then
+    echo "Update found! Pulling new code..."
+    git pull
+    sleep 5
+    echo "Restarting App..."
+    # Restart the service to apply changes
+    sudo systemctl restart garmin.service
+    echo "App updated and restarted."
+else
+    echo "App is already up to date."
+fi
